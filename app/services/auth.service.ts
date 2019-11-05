33dbfaef1f9ae526a25f60d9c9987e50a3d9badf
nagini.bolla <nagini.bolla@wgu.edu>
2019-11-04 19:56:17 -0700

    Angular application using Auth0

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -7,17 +7,17 @@ declare var Auth0Lock: any;
 @Injectable()
 export class Auth {
   // Configure Auth0
-  lock = new Auth0Lock('ZU6zzfUe3ThCC6v8UQaEM69HWzYA1gk6', 'techguyweb.auth0.com', {});
+  lock = new Auth0Lock('fJiBcjsNQDLK7gk9bAGTgKVox1bLD6R5', 'dev-gbbh-3q4.auth0.com', {});
 
   constructor() {
     // Add callback for lock `authenticated` event
-    this.lock.on("authenticated", (authResult:any) => {
-      this.lock.getProfile(authResult.idToken, function(error:any, profile:any){
-          if(error){
+    this.lock.on('authenticated', (authResult: any) => {
+      this.lock.getProfile(authResult.idToken, function(error: any, profile: any){
+          if (error) {
               throw new Error(error);
           }
             localStorage.setItem('id_token', authResult.idToken);
-            localStorage.setItem('profile', JSON.stringify(profile)); 
+            localStorage.setItem('profile', JSON.stringify(profile));
       });
     });
   }
